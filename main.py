7cdd7ca6161fa5c484fe573e1b17b8d72c12ed9d
hamdi <hamdiakippuden@gmail.com>
2025-03-10 20:38:22 +0100

    learning threading

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# initial file
+import socket, threading
+
+server_socket = socket.socket()
+server_socket.bind(('', 3000))
+
+server_socket.listen()
+
+def handle_client(ss: socket):
+    try:
+        response = ss.recv(1024).decode()
+        print(f'Received: {response}')
+    except Exception as e:
+        print(e)
+        ss.close()
+        
+while True:
+    conn, address = server_socket.accept()
+    print(f'Connection from: {address}')
+
+    thread = threading.Thread(target=handle_client, args=(conn,))
+    thread.start()
