4bbd249c5ad6102a3b293129092a09f1769c4a9d
hamdi <hamdiakippuden@gmail.com>
2025-03-17 13:28:21 +0100

    the server side now closes the connection after processing the response

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import socket
+import socket, json
 
 PORT = 3000
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', PORT))
 
 s.listen()
 
-print(f'Listening in on port {PORT}')
+print(f'Listening in on port: {PORT}')
 
 while True:
     conn, address = s.accept()
-    print(f'Connected to {address}')
+    print(f'Connection address: {address}')
 
     response = conn.recv(1024).decode()
 
     if response:
-        data = b'This is the operation result.'
-        conn.send(b'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ' + f'{len(data)}\r\n\r\n'.encode() + data)
+        data = response.split('\r\n').pop()
+        print(json.loads(data))
 
-conn.close()
+        conn.send(b'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 2\r\n\r\nOK')
+
+        conn.close()
