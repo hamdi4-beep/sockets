5142187255e8395dd3da94f16636961051fe70d6
hamdi <hamdiakippuden@gmail.com>
2025-03-18 23:40:09 +0100

    learning to process GET requests

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-import socket, json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 PORT = 3000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', PORT))
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            print(self.headers)
 
-s.listen()
-
-print(f'Listening in on port: {PORT}')
-
-while True:
-    conn, address = s.accept()
-    print(f'Connection address: {address}')
-
-    response = conn.recv(1024).decode()
-
-    if response:
-        data = response.split('\r\n').pop()
-        print(json.loads(data))
-
-        conn.send(b'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 2\r\n\r\nOK')
-
-        conn.close()
+with HTTPServer(('', PORT), RequestHandler) as httpd:
+    httpd.serve_forever()
